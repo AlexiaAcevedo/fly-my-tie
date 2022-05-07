642dff44cfb970c3a041f0e731203ba52df83924
Alexia Acevedo <alexia.acevedo@knights.ucf.edu>
2022-05-07 12:56:24 -0600

    Initial commit

---
+++ flask_app/models/post.py
@@ -0,0 +1,100 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_app.models import user
+
+class Post:
+    def __init__(self, data):
+        self.id = data['id']
+        self.image_url = data['image_url']
+        self.fly_name = data['fly_name']
+        self.pattern_type = data['pattern_type']
+        self.difficulty = data['difficulty']
+        self.hook = data['hook']
+        self.bead = data['bead']
+        self.thread = data['thread']
+        self.fins = data['fins']
+        self.tail = data['tail']
+        self.belly = data['belly']
+        self.body = data['body']
+        self.instructions = data['instructions']
+        self.user_id = data['user_id']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.user = None
+
+    @classmethod
+    def save_post(cls, data):
+        query = 'INSERT INTO posts ( image_url, fly_name, pattern_type, difficulty, hook, bead, thread, fins, tail, belly, body, instructions, user_id, created_at, updated at) VALUES ( %(image_url)s, %(fly_name)s, %(pattern_type)s, %(difficulty)s, %(hook)s, %(bead)s, %(thread)s, %(fins)s, %(tail)s, %(belly)s, %(body)s, %(instructions)s, %(user_id)s, NOW(), NOW() );'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+
+    @classmethod
+    def update_post(cls, data):
+        query = 'UPDATE posts SET image_url = %(image_url)s, fly_name = %(fly_name)s, pattern_type = %(pattern_type)s, difficulty = %(difficulty)s, hook = %(hook)s, bead = %(bead)s, thread = %(thread)s, fins = %(fins)s, tail = %(tail)s, belly = %(belly)s, body = %(body)s, instructions = %(instructions)s, user_id = %(user_id)s, updated_at = NOW() WHERE id = %(id)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+    
+    @classmethod
+    def delete_post(cls, data):
+        query = 'DELETE FROM posts WHERE id = %(id)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+
+    @classmethod
+    def get_all_posts_with_users(cls):
+        query = 'SELECT * FROM posts JOIN users ON users.id = posts.user_id;'
+        results = connectToMySQL('tie_my_fly').query_db(query)
+        if len(results) < 1:
+            return None
+        else:
+            all_posts = []
+            for each_post in results:
+                this_post_instance = cls(each_post)
+                this_user_dictionary = {
+                    "id": each_post['users.id'],
+                    "first_name": each_post['first_name'],
+                    "last_name": each_post['last_name'],
+                    "username": each_post['username'],
+                    "email": each_post['email'],
+                    "password": each_post['password'],
+                    "profile_photo_url": each_post['profile_photo_url'],
+                    "bio": each_post['bio'],
+                    "created_at": each_post['users.created_at'],
+                    "updated_at": each_post['users.updated_at']
+                }
+                post_creator = user.User(this_user_dictionary)
+                this_post_instance.user = post_creator
+                all_posts.append(this_post_instance)
+            return all_posts
+
+    @classmethod
+    def get_one_post_with_user(cls, data):
+        query = 'SELECT * FROM posts JOIN users ON users.id = posts.user_id WHERE posts.id = %(id)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data) 
+        if len(results) < 1:
+            return None
+        else:
+            one_painting = cls(results[0])
+            this_user_dictionary = {
+                "id": results[0]['users.id'],
+                "first_name": results[0]['first_name'],
+                "last_name": results[0]['last_name'],
+                "email": results[0]['email'],
+                "password": results[0]['password'],
+                "created_at": results[0]['users.created_at'],
+                "updated_at": results[0]['users.updated_at']
+            }
+            painting_creator = user.User(this_user_dictionary)
+            one_painting.user = painting_creator
+            return one_painting
+
+
+    @classmethod
+    def get_user_paintings(cls, data):
+        query = "SELECT * FROM users LEFT JOIN paintings ON users.id = paintings.user_id;"
+        results = connectToMySQL('paintings').query_db(query,data)
+        paintings = []
+        for painting in results:
+            paintings.append( cls(painting) )
+        return paintings
+    
---
+++ flask_app/models/user.py
@@ -0,0 +1,102 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import post
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.username = data['username']
+        self.email = data['email']
+        self.password = data['password']
+        self.profile_photo_url = data['profile_photo_url']
+        self.bio = data['bio']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.posts = []
+
+    @classmethod
+    def save_user_initial(cls, data):
+        query = 'INSERT INTO users ( first_name, last_name, username, email, password, created_at, updated_at ) VALUES ( %(first_name)s, %(last_name)s, %(username)s, %(email)s, %(password)s, NOW(), NOW() );'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+
+    @classmethod
+    def save_user_cont(cls, data):
+        query = 'INSERT INTO users ( profile_photo_url, bio ) VALUES ( %(profile_photo_url)s, %(bio)s );'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+    
+    @classmethod
+    def update_user(cls, data):
+        query = 'UPDATE users SET profile_photo_url = %(profile_photo_url)s, first_name = %(first_name)s, last_name = %(last_name)s, bio = %(bio)s WHERE id = %(id)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+
+    @classmethod
+    def delete_user(cls, data):
+        query = 'DELETE FROM users WHERE id = %(id)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return results
+
+    @classmethod
+    def get_by_email(cls, data):
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def get_by_username(cls, data):
+        query = 'SELECT * FROM users WHERE username = %(username)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def get_by_id(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, data)
+        return cls(results[0])
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, user)
+        if len(results) >= 1:
+            flash('Email already taken.', 'register')
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Invalid email format.', 'register')
+            is_valid = False
+        query = 'SELECT * FROM users WHERE username = %(username)s;'
+        results = connectToMySQL('tie_my_fly').query_db(query, user)
+        if len(results) >= 1:
+            flash('Username already taken.', 'register')
+            is_valid = False
+        if not NAME_REGEX.match(user['first_name']):
+            flash('Invalid first name characters.', 'register')
+            is_valid = False
+        if len(user['first_name']) < 2:
+            flash('First name must be at least 2 characters.', 'register')
+            is_valid = False
+        if not NAME_REGEX.match(user['last_name']):
+            flash('Invalid last name characters.', 'register')
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash('Last name must be at least 2 characters.', 'register')
+            is_valid = False
+        if len(user['password']) < 8:
+            flash('Password must be at least 8 characters.', 'register')
+            is_valid = False
+        if user['password'] != user['confirm']:
+            flash('Passwords did not match.', 'register')
+            is_valid = False
+        return is_valid
